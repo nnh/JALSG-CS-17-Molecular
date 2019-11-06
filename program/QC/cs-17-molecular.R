5d5805f86c90ff7c9405f2a855c67fdd9755c24b
kirimichan <mariko.ohtsuka@nnh.go.jp>
2019-11-06 15:22:14 +0900

    Delete Japanese comments

--- program/QC/cs-17-molecular.R
+++ program/QC/cs-17-molecular.R
@@ -63,22 +63,12 @@ exclude_registration_list %>% ReadCsvFiles("utf-8")
 str_c(parent_path, "/input/ext/diseases.csv") %>% ReadCsvFiles("utf-8")
 str_c(parent_path, "/input/ext/facilities.csv") %>% ReadCsvFiles("cp932")
 # ------ Table 1 ------
-# 医療機関名 DM$facility_name
 DM <- raw_DM
 DM$str_SITEID <- formatC(DM$SITEID, width=9, flag="0")
-# DM.SITEID, facilities.csv.A列をキーに facilities.csv.B列を付与
 DM <- DM %>% left_join(raw_facilities, by=c("str_SITEID"="施設コード"))
-# 症例数	DM.SITEID, DM.SUBJID	DM.SITEIDごとのDM.SUBJIDの合計
 table1 <- summarise(group_by(DM, 施設名.ja.), number_of_cases=n())
-# 合計の症例数	DM.SUBJID	DM.SUBJIDの合計を算出
 table1 <- rbind(table1, c("合計", sum(table1$number_of_cases)))
 # ------ Table 2 ------
-# 症例番号
-# CS-17-Molecular_registration_yyyymmdd_mmss.csv.症例登録番号
-# CS-17_registration_yyyymmdd_mmss.csv.登録コードが存在するもののみ出力
-# JALSG番号
-# CS-17-Molecular_registration_yyyymmdd_mmss.csv.登録コード
-# CS-17_registration_yyyymmdd_mmss.csv.登録コードが存在するもののみ出力
 local({
   df_registration <- get(kCS_17) %>% select(c(症例登録番号, 登録コード))
   df_molecular <- get(kCS_17_Molecular) %>% select("登録コード") %>% right_join(df_registration, by=c("登録コード"))
@@ -86,19 +76,9 @@ local({
                left_join(df_registration, by=c("SUBJID"="症例登録番号")) %>%
                    left_join(df_molecular, by="登録コード")
 })
-# 年齢（診断時）
-# MH.MHDTC, DM.BRTHDTC
-# MH.MHDTC where MHCAT=PRIMARY DIAGNOSISとDM.BRTHDTCから算出
 table2 <- raw_MH %>% select(c(USUBJID, MHDTC)) %>% filter(MHDTC != "") %>% inner_join(table2, by="USUBJID")
 table2$age <- apply(table2, 1, function(x){return(length(seq(as.Date(x["BRTHDTC"]), as.Date(x["MHDTC"]), "year")) - 1)})
-# 性別
-# DM.SEX
-# "If DM.SEX=0 then M
-# Else F"
 table2$str_sex <- ifelse(table2$SEX == 0, "M", "F")
-# WHO2016 診断病名
-# MH.MHTERM, MH.MHCAT
-# MH.MHTERM where MHCAT=PRIMARY DIAGNOSIS
 MH <- raw_MH %>% select(c(USUBJID, MHTERM, MHCAT, MHDTC)) %>% filter(MHCAT=="PRIMARY DIAGNOSIS")
 local({
   df_diseases <- raw_diseases
@@ -106,10 +86,6 @@ local({
   table2 <<- MH %>% select(c(USUBJID, MHTERM)) %>% right_join(table2, by="USUBJID") %>%
             left_join(df_diseases, by=c("MHTERM"="str_code"))
 })
-# 投与時期
-# EC.EPOCH EC.ECTPT
-# 治療内容
-# EC.ECTRT
 epoch_order <- data.frame(c(1, 2, 3),
                           c("INDUCTION THERAPY", "CONSOLIDATION THERAPY/MAINTENANCE THERAPY", "SALVAGE THERAPY"),
                           c("寛解導入療法", "地固め療法・維持療法", "サルベージ療法"),
@@ -117,7 +93,6 @@ epoch_order <- data.frame(c(1, 2, 3),
 colnames(epoch_order) <- c("epoch_order", "EPOCH", "epoch_ja")
 table2 <- raw_EC %>% left_join(epoch_order, by="EPOCH") %>%
             select(c(USUBJID, EPOCH, ECTPT, ECTRT, epoch_order, epoch_ja, ECSPID)) %>% right_join(table2, by="USUBJID")
-# 日本語ラベル
 ectpt_label <- data.frame(c("COURSE 1", "COURSE 2", "COURSE 3", "COURSE 4"),
                           c("1コース", "2コース", "3コース", "4コース"), stringsAsFactors=F)
 colnames(ectpt_label) <- c("ECTPT", "ectpt_ja")
@@ -130,38 +105,20 @@ table2$epoch_ja <- apply(table2, 1, function(x){
   }
   return(res)
 })
-# 寛解もしくは安定の有無
-# RS.RSORRES, RS.EPOCH
-# "If RS.RSORRES where EPOCH=INDUCTION THERAPY in (CR, CRi) then 有
-# Else 無"
 table2 <- raw_RS %>% select(c(USUBJID, RS_EPOCH="EPOCH", RSORRES, RSDTC, RSSPID)) %>%
             right_join(table2, by=c("USUBJID", "RS_EPOCH"="EPOCH"))
 table2$remission <- ifelse((table2$RS_EPOCH == "INDUCTION THERAPY") & (table2$RSORRES == "CR" | table2$RSORRES == "CRi"),
                            "有", NA)
-# 再発もしくは増悪の有無*1
-# RS.RSORRES, RS.EPOCH, CE.CEDTC
-# "寛解もしくは安定の有無が有について
-# If CE.CEDTC^=Null 有
-# Else 無"
 table2 <- raw_CE %>% select(c(USUBJID, CEDTC)) %>% group_by(USUBJID) %>% filter(CEDTC == min(CEDTC)) %>%
             right_join(table2, by="USUBJID")
 table2$remission <- ifelse(is.na(table2$remission), "無", "有")
 table2$relapse <- ifelse(table2$remission == "無", NA, ifelse(!is.na(table2$CEDTC), "有", "無"))
-# 寛解日数もしくは無増悪期間*1
-# RS.RSORRES, RS.EPOCH, RS.RSDTC, CE.CEDTC
-# "寛解もしくは安定の有無が有について
-# 最初のCE.CEDTC-RS.RSDTC where (EPOCH=INDUCTION THERAPY and RS.RSORRES in (CR, CRi)) +1"
 table2$days_of_remission <- ifelse(table2$remission == "無", NA, as.Date(table2$CEDTC) - as.Date(table2$RSDTC) + 1)
-# 生存日数
-# DS.DSSTDTC, DS.DSTERM, MH.MHDTC, MH.MHCAT
-# "死亡症例について
-# DS.DSSTDTC where DSTERM=DEATH - MH.MHDTC where MHCAT=PRIMARY DIAGNOSIS+1"
 DS <- raw_DS %>% inner_join(MH, by="USUBJID") %>%
         select(c(USUBJID, DSSTDTC, DSTERM, MHDTC, MHCAT)) %>% filter(DSTERM == "DEATH")
 DS$survival_days <- as.Date(DS$DSSTDTC) - as.Date(DS$MHDTC) + 1
 table2 <- table2 %>% left_join(DS, by="USUBJID")
-# 症例番号、JALSG番号昇順でソート
-# EC.ECTPTで昇順
+# sort
 table2 <- table2 %>% arrange(USUBJID, 登録コード, epoch_order, ECTPT) %>% distinct()
 output_table2 <- table2 %>% select(c(USUBJID, 登録コード, 施設名.ja., age, str_sex, name_en, epoch_ja, ECTRT, remission,
                                      relapse, days_of_remission, survival_days)) %>% distinct()
