72f6c749f3221af2fa7fe38e1d0334c5cea91bd3
kirimichan <mariko.ohtsuka@nnh.go.jp>
2020-06-18 13:41:48 +0900

    Exclude directories from the input file list

--- program/QC/cs-17-molecular.R
+++ program/QC/cs-17-molecular.R
@@ -56,7 +56,7 @@ AddStyleTable2 <- function(output_wb, target_sheet_name, target_row, border_styl
 # ------ init ------
 parent_path <- "/Volumes/Stat/Trials/JAGSE/CS-17-Molecular_interim"
 # read csv
-file_list <- str_c(parent_path, "/input/rawdata") %>% list.files(full.names=T, pattern=".csv") # Exclude folders
+file_list <- str_c(parent_path, "/input/rawdata") %>% list.files(full.names=T, pattern=".csv") # Exclude directories
 registration_list <- str_subset(file_list, pattern="([^/]+)(?=_[0-9]{6}_[0-9]{4}.csv)")
 exclude_registration_list <- file_list[-which(file_list %in% registration_list)]
 registration_list %>% ReadCsvFiles("cp932")
